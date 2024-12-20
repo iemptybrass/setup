052f2e0aff54f5b69d3d86ac9383155e46efc872
iemptybrass <187961073+iemptybrass@users.noreply.github.com>
2024-12-20 16:54:50 -0500

    Create hardware-configuration.nix

---
+++ | NixOS |/etc/nixos/hosts/default/hardware-configuration.nix
@@ -0,0 +1,50 @@
+ { config, lib, pkgs, modulesPath, ... }:
+
+
+
+{
+
+
+
+
+  
+  imports = [ ( modulesPath + " " ) ];
+
+
+  
+  boot = {
+      kernelModules = [ ];
+      extraModulePackages = [ ];
+    initrd = {
+        availableKernelModules = [ ];
+        kernelModules = [ ];      }; 
+          };
+
+
+
+  fileSystems = {
+    "/" = 
+        { device = "/dev/disk/by-uuid/UUID";
+          fsType = "ext4";      };
+    "/boot" =
+        { device = "/dev/disk/by-uuid/UUID";
+          fsType = "vfat";
+          optiosn = [ "fmask=0077" "dmask=0077" ];      };
+                 };
+  swapDevices = [ ];
+
+
+
+  networking.useDHCP = lib.mkDefault true;
+
+
+
+  nixpkgs.hostPlatform = lib.mkDefault "*";
+
+
+
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+
+
+}
