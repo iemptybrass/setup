b40c73bb17e9d89613fbac63d875da17ad480417
iemptybrass <187961073+iemptybrass@users.noreply.github.com>
2024-12-24 20:35:31 -0500

    Update configuration.nix

--- | NixOS |/etc/nixos/hosts/default/configuration.nix
+++ | NixOS |/etc/nixos/hosts/default/configuration.nix
@@ -69,6 +69,16 @@ system.stateVersion = "24.11";
 };
 
 
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal"; # Without this errors will spam on screen
+    # Without these bootlogs will spam on screen
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
+  };
 
   xdg.portal = {
       enable = true;
