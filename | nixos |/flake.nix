4cb470e05588911c25b822238ceff112e914911b
iemptybrass <187961073+iemptybrass@users.noreply.github.com>
2024-12-25 18:18:43 -0500

    Update flake.nix

--- | nixos |/flake.nix
+++ | nixos |/flake.nix
@@ -16,6 +16,9 @@
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";      };
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";      };
             };
 
 
