14133fb317b6b1766b2f5818862ef2114ba9beb5
iemptybrass <187961073+iemptybrass@users.noreply.github.com>
2024-12-26 13:36:02 -0500

    Update flake.nix

--- | nixos |/flake.nix
+++ | nixos |/flake.nix
@@ -12,7 +12,7 @@
 
 
   inputs = {
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";      };
