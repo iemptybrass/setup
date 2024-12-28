56ca707581f00bd0be40b9f1a70fffb6b30e4a43
iemptybrass <187961073+iemptybrass@users.noreply.github.com>
2024-12-28 17:26:42 -0500

    Update flake.nix

--- | nixos |/flake.nix
+++ | nixos |/flake.nix
@@ -7,17 +7,12 @@
 
 
 
-  description = "Nixos config flake";
-
-
-
   inputs = {
       nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
         url = "github:nix-community/home-manager";
         inputs.nixpkgs.follows = "nixpkgs";      };
-    hyprland = {
-        url = "github:hyprwm/Hyprland";      };
+    hyprland.url = "github:hyprwm/Hyprland";
             };
 
 
