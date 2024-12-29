2821ca142ffe8f130010bb053cd1e89f9063b4d0
iemptybrass <187961073+iemptybrass@users.noreply.github.com>
2024-12-29 09:16:44 -0500

    Update configuration.nix

--- | nixos |/configuration.nix
+++ | nixos |/configuration.nix
@@ -89,8 +89,8 @@ system.stateVersion = "24.11";
 
 
 hardware.uinput.enable = true; #
-users.groups.uinput.members = [user]; #
-users.groups.input.members = [user]; #
+users.groups.uinput.members = [ "user" ]; #
+users.groups.input.members = [ "user" ]; #
 
 
 
