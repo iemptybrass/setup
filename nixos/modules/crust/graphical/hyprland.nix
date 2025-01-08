5b1d7145496b6fd1b5a6d572ab9f09e961128176
iemptybrass <187961073+iemptybrass@users.noreply.github.com>
2025-01-08 18:28:00 -0500

    Update hyprland.nix

--- nixos/modules/crust/graphical/hyprland.nix
+++ nixos/modules/crust/graphical/hyprland.nix
@@ -10,12 +10,12 @@
 
   wayland.windowManager.hyprland = {
       enable = true;
-      systemd.enable = false;
     settings = {
 
 
 
-      exec-once = "waybar & nm-applet & dunst";
+      exec-once = "waybar & nm-applet & dunst &";
+
 
       monitor = "eDP-1, 1920x1080@60, 0x0, 1";
 
@@ -67,28 +67,29 @@
                     };
 
 
-  
+
       dwindle = {
           pseudotile = "true";
           preserve_split = "true";
                  };
-  
+
 
 
       master = {
           new_status = "master";
                 };
-  
-  
-    
+
+
+
       misc = {
+          vfr = true;
           force_default_wallpaper = "0";
           disable_hyprland_logo = "true";
           disable_hyprland_qtutils_check = "true";
               };
-    
-    
-    
+
+
+
       input = {
           kb_layout = "us";
           follow_mouse = "1";
@@ -96,75 +97,71 @@
           touchpad = {
               natural_scroll = false;      };
                };
-    
-    
-    
+
+
+
       "$mainMod" = "SUPER";
-    
+
       bind = [
-          "$mainMod, Q, exec, $terminal"
-          "$mainMod, C, killactive,"
-          "$mainMod, M, exit,"
-          "$mainMod, E, exec, $fileManager"
-          "$mainMod, V, togglefloating,"
-          "$mainMod, R, exec, $menu"
+          "$mainMod, Q, killactive,"
           "$mainMod, P, pseudo,"
           "$mainMod, J, togglesplit,"
-    
-          "$mainMod, left, movefocus, l"
-          "$mainMod, right, movefocus, r"
-          "$mainMod, up, movefocus, u"
-          "$mainMod, down, movefocus, d"
-    
+          "$mainMod, E, exit,"
+
           "$mainMod, 1, workspace, 1"
           "$mainMod, 2, workspace, 2"
           "$mainMod, 3, workspace, 3"
           "$mainMod, 4, workspace, 4"
           "$mainMod, 5, workspace, 5"
-          "$mainMod, 6, workspace, 6"
-          "$mainMod, 7, workspace, 7"
-          "$mainMod, 8, workspace, 8"
-          "$mainMod, 9, workspace, 9"
-    
+
           "$mainMod SHIFT, 1, movetoworkspace, 1"
           "$mainMod SHIFT, 2, movetoworkspace, 2"
           "$mainMod SHIFT, 3, movetoworkspace, 3"
           "$mainMod SHIFT, 4, movetoworkspace, 4"
           "$mainMod SHIFT, 5, movetoworkspace, 5"
-          "$mainMod SHIFT, 6, movetoworkspace, 6"
-          "$mainMod SHIFT, 7, movetoworkspace, 7"
-          "$mainMod SHIFT, 8, movetoworkspace, 8"
-          "$mainMod SHIFT, 9, movetoworkspace, 9"
-    
-          "$mainMod, S, togglespecialworkspace, magic"
-          "$mainMod SHIFT, S, movetoworkspace, special:magic"
-    
-          "$mainMod, mouse_down, workspace, e+1"
-          "$mainMod, mouse_up, workspace, e-1"      ];
-    
+
+          "$mainMod, APOSTROPHE, togglespecialworkspace, toggle"
+          "$mainMod SHIFT, APOSTROPHE, movetoworkspace, special:toggle"      
+
+          ", XF86AudioNext, exec, playerctl next"
+          ", XF86AudioPause, exec, playerctl play-pause"
+          ", XF86AudioPlay, exec, playerctl play-pause"
+          ", XF86AudioPrev, exec, playerctl previous"      ];
+
+bindo = [
+
+                ]
+
+
+
+      bindr = [
+          "$mainMod, W, exec, pkill $menu|$menu"
+               ];
+
+
       bindm = [
           "$mainMod, mouse:272, movewindow"
           "$mainMod, mouse:273, resizewindow"      ];
-    
-      bindel = [
+
+      binde = [
           ",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
           ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
           ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
           ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
           ",XF86MonBrightnessUp, exec, brightnessctl s 10%+"
           ",XF86MonBrightnessDown, exec, brightnessctl s 10%-"      ];
-    
-      bindl = [
-          ", XF86AudioNext, exec, playerctl next"
-          ", XF86AudioPause, exec, playerctl play-pause"
-          ", XF86AudioPlay, exec, playerctl play-pause"
-          ", XF86AudioPrev, exec, playerctl previous"      ];
-    
+
+
+
+      workspace = [
+          "3, monitor:eDP-1, default:true, persistent:true"
+                   ];
+
       windowrulev2 = [
           "suppressevent maximize, class:.*"
           "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"      ];
-      
-  
+
+
 
                 };      };
 
