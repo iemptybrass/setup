099a47b7d19d8dc9fa18b1ef5657676920f24746
iemptybrass <187961073+iemptybrass@users.noreply.github.com>
2025-01-09 10:20:48 -0500

    Update wofi.nix

--- nixos/modules/crust/graphical/wofi.nix
+++ nixos/modules/crust/graphical/wofi.nix
@@ -14,6 +14,15 @@
 
   programs.wofi = {
       enable = true;
+    settings = {
+        width = "25%";
+        height = "50%";
+        prompt = "Search ";
+        allow_images = true;
+        term = "ghostty";
+        hide_scroll = true;
+        insensitive = true;
+                };
                    };
 
 
