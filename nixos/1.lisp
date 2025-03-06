6201309588d91aaa9dd05dfb2d9edd5612201bcf
iemptybrass <187961073+iemptybrass@users.noreply.github.com>
2025-03-06 12:15:15 -0500

    Update 1.lisp

--- nixos/1.lisp
+++ nixos/1.lisp
@@ -1,43 +1,48 @@
-(define-configuration (window)
-    ((status-buffer-position :top)))
-    
+(sb-ext:unlock-package :nyxt)
+(define-setf-handler (class-name slot bound-object handler)
+
+
+
+#+nyxt-3
+(defmethod format-status ((status status-buffer))
+  (let ((buffer (current-buffer (window status))))
+;;    (setf (style status) (my-status-style))
+    (spinneret:with-html-string
+      (:div :id "container"
+            (:div :id "controls" :class "arrow-right"
+                  (:raw (nyxt::my-format-status-buttons)))
+            ;; (:div :class "arrow arrow-right"
+            ;;       :style "background-color:rgb(21,21,21);background-color:rgb(49,49,49)"  "")
+            (:div :id "url"
+                  (:raw
+                   (nyxt::format-status-load-status status)
+                   (nyxt::my-format-status-url buffer)))
+	    (:div :id "tag" :class "arrow-left"
+		  (:raw
+		   (nyxt::format-status-tag buffer)))
+            ;; (:div :class "arrow arrow-left"
+            ;;       :style "background-color:rgb(21,21,21);background-color:rgb(49,49,49)" "")
+            (:div :id "modes"
+		  :title (nyxt::modes-string buffer)
+		  "--")))))
+
 (in-package #:nyxt-user)
 
-(define-configuration :status-buffer
-  ((height 30))
-  ((glyph-mode-presentation-p t)))
+(define-configuration (window)
+    ((status-buffer-position :top)))
 
 
 
 
 
-(define-configuration :force-https-mode ((glyph "ϕ")))
-(define-configuration :user-script-mode ((glyph "u")))
-(define-configuration :blocker-mode ((glyph "β")))
-(define-configuration :proxy-mode ((glyph "π")))
-(define-configuration :reduce-tracking-mode ((glyph "τ")))
-(define-configuration :certificate-exception-mode ((glyph "χ")))
-(define-configuration :style-mode ((glyph "ϕ")))
-(define-configuration :cruise-control-mode ((glyph "σ")))
 
-;;(define-configuration status-buffer
-;;  "Hide most of the status elements but URL and modes."
-;;  ((style (str:concat
-;;           %slot-value%
-;;           (theme:themed-css (theme *browser*)
-;;	     `("#controls,#tabs"
-;;	       :display none !important))))))
 
-(defmethod format-status-load-status ((status status-buffer))
-  "A fancier load status."
-  (spinneret:with-html-string
-   (:span (if (and (current-buffer)
-                   (web-buffer-p (current-buffer)))
-              (case (slot-value (current-buffer) 'nyxt::status)
-                    (:unloaded "∅")
-                    (:loading "∞")
-                    (:finished ""))
-            ""))))
+
+
+
+
+echo "it worked"
+
 
 
 
